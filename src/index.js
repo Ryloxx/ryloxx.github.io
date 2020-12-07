2d83458ce1291b4af5a12a37cd48eb58b63c4aaa
Ryloxx <in.telo3200@gmail.com>
2020-12-07 13:28:26 +0100

    disable animations for phones

--- src/index.js
+++ src/index.js
@@ -81,46 +81,8 @@ function makeCard({ image, title, body, techList, link, theme }) {
   wrap.appendChild(card);
   return wrap;
 }
-let globalTimeoutId = null;
-function observerHandler(card) {
-  // const timeoutBeforeExpand = 500;
-  const maxRatioThreshold = 0.8;
-  const maxTranslate = 15;
-  const deadZone = 0.1;
-  const scaleVariant = 0.1;
-  const opacityVariant = 1;
-  const translateVariant = 1;
-  /* Multiplier will vary from 0 to 1 with a giver ratio lower or equal to 1*/
-  const getMultiplier = (ratio) =>
-    Math.max(
-      0,
-      Math.min(1, (ratio - deadZone) * (1 / (maxRatioThreshold - deadZone))),
-    );
-  return (entries) => {
-    const entry = entries[0];
-    if (entry.intersectionRatio < 1 - maxRatioThreshold) {
-      card.classList.remove('expanded');
-    }
-    const multi = getMultiplier(entry.intersectionRatio);
-    const fixedTranslate = maxTranslate * (1 - translateVariant);
-    const variableTranslate = maxTranslate - fixedTranslate;
-    requestAnimationFrame(() => {
-      card.style.opacity = 1 - opacityVariant + opacityVariant * multi;
-      card.style.transform = `scale(${
-        1 - scaleVariant + scaleVariant * multi
-      }) translate(-${
-        maxTranslate - (fixedTranslate + variableTranslate * multi)
-      }%)`;
-    });
-  };
-}
+
 function processCard(card) {
-  const pollRate = 100;
-  const observer = new IntersectionObserver(observerHandler(card), {
-    threshold: Array(pollRate + 1)
-      .fill()
-      .map((_, index) => index / pollRate),
-  });
   card.addEventListener('click', () => {
     if (card.classList.contains('expanded')) {
       card.classList.remove('expanded');
@@ -139,10 +101,9 @@ function processCard(card) {
       scrollEndHandler();
     }
   });
-  observer.observe(card);
-  requestAnimationFrame(() => {
-    card.style.opacity = 1;
-  });
+  if (window.observeCard) {
+    window.observeCard(card);
+  }
 }
 function defer(fnc, timeout) {
   setTimeout(() => {
@@ -155,18 +116,21 @@ function makeGridCard(cardDataList) {
     defer(() => {
       const card = makeCard(cardData);
       processCard(card);
-      // workaround for the issue that hides the first card on page load even if it is inside viewport
       requestAnimationFrame(() => gridElem.appendChild(card));
     }, index * 100);
   });
-  requestAnimationFrame(() => gridElem.classList.add('card-grid'));
+  gridElem.classList.add('card-grid');
   return gridElem;
 }
-
 const app = document.getElementById('root');
-fetch('./projectData.json')
-  .then((response) => response.json())
-  .then((json) => {
-    const grid = makeGridCard(json);
-    app.appendChild(grid);
-  });
+app.ready = new Promise((resolve) => {
+  fetch('./projectData.json')
+    .then((response) => response.json())
+    .then((json) => {
+      const grid = makeGridCard(json);
+      requestAnimationFrame(() => {
+        app.appendChild(grid);
+      });
+      resolve(app);
+    });
+});
