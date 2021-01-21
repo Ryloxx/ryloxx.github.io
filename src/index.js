e6507841d24d26d70ae4c905e5bf3b946400a5cc
Ryloxx <in.telo3200@gmail.com>
2021-01-21 20:34:33 +0100

    before v3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
 import Background from './bar-background/index.js';
 let background = null;
 function getClientX(event) {
